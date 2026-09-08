d7474b479dbabed34db1d34da50a452fab3054cc
Lucas Assis Rodrigues <lucas.assis.ro@gmail.com>
2026-09-07 22:30:55 -0400

    feat: conform the remaining cancelable Foundation types
    Adds `OperationQueue`, `Progress`, `Port`, `Thread`, `URLSession` and
    `XMLParser`, along with `NSFileCoordinator`, `NSUserActivity`,
    `NSMetadataQuery`, `NSBundleResourceRequest`, `NetService` and
    `NetServiceBrowser` where Darwin is available, and
    `NSBackgroundActivityScheduler`, `NSXPCConnection` and `NSXPCListener` on macOS.

--- Sources/CancelableOnDeinit/CancelableOnDeinit+Foundation.swift
+++ Sources/CancelableOnDeinit/CancelableOnDeinit+Foundation.swift
@@ -8,6 +8,12 @@
     }
   }
 
+  extension OperationQueue: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      cancelAllOperations()
+    }
+  }
+
   extension DispatchWorkItem: CancelableOnDeinit {
     /// Cancels the work item.
     public func cancelOnDeinit() {
@@ -22,10 +28,102 @@
     }
   }
 
+  extension Progress: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      cancel()
+    }
+  }
+
+  extension Port: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      invalidate()
+    }
+  }
+
+  extension Thread: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      cancel()
+    }
+  }
+
+  extension URLSession: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      invalidateAndCancel()
+    }
+  }
+
   extension URLSessionTask: CancelableOnDeinit {
     /// Cancels the task, failing it with `NSURLErrorCancelled`.
     public func cancelOnDeinit() {
       cancel()
     }
   }
+
+  extension XMLParser: CancelableOnDeinit {
+    public func cancelOnDeinit() {
+      abortParsing()
+    }
+  }
+
+  #if canImport(Darwin)
+    extension NSFileCoordinator: CancelableOnDeinit {
+      public func cancelOnDeinit() {
+        cancel()
+      }
+    }
+
+    extension NSUserActivity: CancelableOnDeinit {
+      public func cancelOnDeinit() {
+        invalidate()
+      }
+    }
+
+    extension NSMetadataQuery: CancelableOnDeinit {
+      public func cancelOnDeinit() {
+        stop()
+      }
+    }
+
+    #if !os(macOS)
+      extension NSBundleResourceRequest: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          endAccessingResources()
+        }
+      }
+    #endif
+
+    #if !os(watchOS)
+      extension NetService: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          stop()
+        }
+      }
+
+      extension NetServiceBrowser: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          stop()
+        }
+      }
+    #endif
+
+    #if os(macOS)
+      extension NSBackgroundActivityScheduler: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          invalidate()
+        }
+      }
+
+      extension NSXPCConnection: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          invalidate()
+        }
+      }
+
+      extension NSXPCListener: CancelableOnDeinit {
+        public func cancelOnDeinit() {
+          invalidate()
+        }
+      }
+    #endif
+  #endif
 #endif
